382df4ca2e7e2301d2f8583ba162b97038adf6de
fanlelee <529378687@qq.com>
2021-10-19 21:00:00 +0800

    配置不同情况的webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-    mode: "production",
     entry: {
         index: './lib/index.tsx' //入口文件
     },
@@ -21,25 +20,5 @@ module.exports = {
                 loader: 'awesome-typescript-loader'
             }
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "FUI",
-            template: "index.html"
-        })
-    ],
-    externals: {
-        react: {
-            commonjs: 'react',
-            commonjs2: 'react',
-            amd: 'react',
-            root: 'React',
-        },
-        'react-dom': {
-            commonjs: 'react-dom',
-            commonjs2: 'react-dom',
-            amd: 'react-dom',
-            root: 'ReactDOM',
-        },
     }
 }
