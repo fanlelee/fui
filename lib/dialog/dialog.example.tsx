3d21fa1c7f70fcf73254dd53d76d88713b0da11e
fanlelee <529378687@qq.com>
2022-02-24 11:43:09 +0800

    dialog加事件效果

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -3,10 +3,21 @@ import Dialog from './dialog';
 
 const DialogExample: React.FunctionComponent = () => {
     const [status, setStatus] = useState(false);
+
     return (
         <>
             <button onClick={() => {setStatus(i => !i);}}>dialog toggle</button>
-            <Dialog visible={status}>
+            <Dialog
+                visible={status}
+                buttons={
+                    [
+                        <button onClick={() => setStatus(false)}>取消</button>,
+                        <button onClick={() => setStatus(false)}>确定</button>
+                    ]
+                }
+                onClickClose={() => setStatus(false)}
+                closeOnClickMask={true}
+            >
                 <div>去不去吃饭？</div>
             </Dialog>
         </>
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, {ReactElement} from 'react';
 import './dialog.scss';
 import Icon from '../icon/icon';
 
 interface DialogProps {
-    visible: boolean
+    visible: boolean,
+    buttons?: Array<ReactElement>,
+    onClickClose: React.MouseEventHandler,
+    closeOnClickMask?: boolean
 }
 
 function scopedClassMaker(prefix: string) {
-    return (name?: string)=> {
+    return (name?: string) => {
         return ['fui', prefix, name].filter(Boolean).join('-');
     };
 }
+
 const sc = scopedClassMaker('dialog');
 
 const Dialog: React.FunctionComponent<DialogProps> = (props) => {
+    const onClickMask:React.MouseEventHandler = (e)=>{
+        if(props.closeOnClickMask){props.onClickClose(e)}
+    }
     return (
         props.visible ?
             <>
-                <div className={sc('mask')}/>
+                <div
+                    className={sc('mask')}
+                    onClick={onClickMask}
+                />
                 <div className={sc()}>
-                    <div className={sc('close')}><Icon name='close'/></div>
+                    <div
+                        className={sc('close')}
+                        onClick={props.onClickClose}
+                    >
+                        <Icon name='close'/>
+                    </div>
                     <header className={sc('header')}>
                         信息提示
                     </header>
@@ -27,8 +42,12 @@ const Dialog: React.FunctionComponent<DialogProps> = (props) => {
                         {props.children}
                     </main>
                     <footer className={sc('footer')}>
-                        <button className={sc('cancel')}>取消</button>
-                        <button className={sc('ok')}>确定</button>
+                        {
+                            props.buttons &&
+                            props.buttons.map((button, index) =>
+                                React.cloneElement(button, {key: index})
+                            )
+                        }
                     </footer>
                 </div>
             </>
