ffc664d30889eafd81b642f5fa36f25591ae071f
fanlelee <529378687@qq.com>
2022-02-24 10:51:18 +0800

    dialog加样式

--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
 import './dialog.scss';
-import Icon from '../icon/icon'
+import Icon from '../icon/icon';
 
 interface DialogProps {
     visible: boolean
 }
 
+function scopedClassMaker(prefix: string) {
+    return (name?: string)=> {
+        return ['fui', prefix, name].filter(Boolean).join('-');
+    };
+}
+const sc = scopedClassMaker('dialog');
+
 const Dialog: React.FunctionComponent<DialogProps> = (props) => {
     return (
         props.visible ?
             <>
-                <div className='fui-dialog-mask'/>
-                <div className='fui-dialog'>
-                    <div><Icon name='close'/></div>
-                    <header>信息提示</header>
-                    <main>
+                <div className={sc('mask')}/>
+                <div className={sc()}>
+                    <div className={sc('close')}><Icon name='close'/></div>
+                    <header className={sc('header')}>
+                        信息提示
+                    </header>
+                    <main className={sc('main')}>
                         {props.children}
                     </main>
-                    <footer>
-                        <button>取消</button>
-                        <button>确定</button>
+                    <footer className={sc('footer')}>
+                        <button className={sc('cancel')}>取消</button>
+                        <button className={sc('ok')}>确定</button>
                     </footer>
                 </div>
             </>
