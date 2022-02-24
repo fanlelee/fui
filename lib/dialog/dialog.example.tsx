19fc11c604775a1613672412688b720ae5c2c8dc
fanlelee <529378687@qq.com>
2022-02-24 16:24:27 +0800

    用portal将dialog放进body里

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -3,23 +3,43 @@ import Dialog from './dialog';
 
 const DialogExample: React.FunctionComponent = () => {
     const [status, setStatus] = useState(false);
+    const [status2, setStatus2] = useState(false);
 
     return (
         <>
-            <button onClick={() => {setStatus(i => !i);}}>dialog toggle</button>
-            <Dialog
-                visible={status}
-                buttons={
-                    [
-                        <button onClick={() => setStatus(false)}>取消</button>,
-                        <button onClick={() => setStatus(false)}>确定</button>
-                    ]
-                }
-                onClickClose={() => setStatus(false)}
-                closeOnClickMask={true}
-            >
-                <div>去不去吃饭？</div>
-            </Dialog>
+            <div>
+                <button onClick={() => {setStatus(i => !i);}}>dialog toggle</button>
+                <Dialog
+                    visible={status}
+                    buttons={
+                        [
+                            <button onClick={() => setStatus(false)}>取消</button>,
+                            <button onClick={() => setStatus(false)}>确定</button>
+                        ]
+                    }
+                    onClickClose={() => setStatus(false)}
+                    closeOnClickMask={true}
+                >
+                    <div>去不去吃饭？</div>
+                </Dialog>
+            </div>
+
+            <div>
+                <button onClick={() => {setStatus2(i => !i);}}>dialog2</button>
+                <Dialog
+                    visible={status2}
+                    buttons={
+                        [
+                            <button onClick={() => setStatus2(false)}>取消2</button>,
+                            <button onClick={() => setStatus2(false)}>确定2</button>
+                        ]
+                    }
+                    onClickClose={() => setStatus2(false)}
+                    closeOnClickMask={true}
+                >
+                    <div>去不去吃饭？2</div>
+                </Dialog>
+            </div>
         </>
     );
 };
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,6 +1,7 @@
 import React, {ReactElement} from 'react';
 import './dialog.scss';
 import Icon from '../icon/icon';
+import ReactDOM from 'react-dom';
 
 interface DialogProps {
     visible: boolean,
@@ -21,37 +22,38 @@ const Dialog: React.FunctionComponent<DialogProps> = (props) => {
     const onClickMask:React.MouseEventHandler = (e)=>{
         if(props.closeOnClickMask){props.onClickClose(e)}
     }
-    return (
-        props.visible ?
-            <>
+    const res = props.visible ?
+        <>
+            <div
+                className={sc('mask')}
+                onClick={onClickMask}
+            />
+            <div className={sc()}>
                 <div
-                    className={sc('mask')}
-                    onClick={onClickMask}
-                />
-                <div className={sc()}>
-                    <div
-                        className={sc('close')}
-                        onClick={props.onClickClose}
-                    >
-                        <Icon name='close'/>
-                    </div>
-                    <header className={sc('header')}>
-                        信息提示
-                    </header>
-                    <main className={sc('main')}>
-                        {props.children}
-                    </main>
-                    <footer className={sc('footer')}>
-                        {
-                            props.buttons &&
-                            props.buttons.map((button, index) =>
-                                React.cloneElement(button, {key: index})
-                            )
-                        }
-                    </footer>
+                    className={sc('close')}
+                    onClick={props.onClickClose}
+                >
+                    <Icon name='close'/>
                 </div>
-            </>
-            : null
+                <header className={sc('header')}>
+                    信息提示
+                </header>
+                <main className={sc('main')}>
+                    {props.children}
+                </main>
+                <footer className={sc('footer')}>
+                    {
+                        props.buttons &&
+                        props.buttons.map((button, index) =>
+                            React.cloneElement(button, {key: index})
+                        )
+                    }
+                </footer>
+            </div>
+        </>
+        : null
+    return (
+        ReactDOM.createPortal(res, document.body)
     );
 };
 export default Dialog;
