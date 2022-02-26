325eb7a692774a81ddfc043bf43b52e4b3667db1
fanlelee <529378687@qq.com>
2022-02-26 22:00:30 +0800

    scopedClassMaker更加函数式了

--- lib/helpers/classes.ts
+++ lib/helpers/classes.ts
@@ -2,17 +2,14 @@ interface ClassToggles {
     [K: string]: boolean
 }
 
-function scopedClassMaker(prefix: string) {
-    return (name: string | ClassToggles, extra?: string) => {
-        const namesObject = typeof name === 'string' ? {[name]: true} : name;
-        return  Object.entries(namesObject)
+const scopedClassMaker = (prefix: string) =>
+    (name: string | ClassToggles, extra?: string) =>
+        Object.entries(typeof name === 'string' ? {[name]: true} : name)
             .filter(k => k[1])
             .map(k => k[0])
             .map(k => ['fui', prefix, k].filter(Boolean).join('-'))
-            .concat(extra?[extra]:[])
+            .concat(extra ? [extra] : [])
             .join(' ');
 
-    };
-}
 
 export {scopedClassMaker};
