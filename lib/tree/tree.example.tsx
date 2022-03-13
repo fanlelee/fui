add03b90d5b4f11bcfe85f9519318842b7efb777
fanlelee <529378687@qq.com>
2022-03-13 16:40:16 +0800

    tree 可 单选

--- lib/tree/tree.example.tsx
+++ lib/tree/tree.example.tsx
@@ -3,41 +3,45 @@ import Tree, {SourceDataItem} from './tree';
 
 
 const TreeExample: React.FunctionComponent = () => {
-    const [sourceData]  = useState([
+    const [sourceData] = useState([
         {
-            text:'一',
-            value:'1',
-            children:[
-                {text:'一.一', value:'1.1'},
-                {text:'一.二', value:'1.2'}
+            text: '一',
+            value: '1',
+            children: [
+                {text: '一.一', value: '1.1'},
+                {text: '一.二', value: '1.2'}
             ]
         },
         {
-            text:'二',
-            value:'2',
-            children:[
-                {text:'二.一', value:'2.1'},
-                {text:'二.二', value:'2.2'}
+            text: '二',
+            value: '2',
+            children: [
+                {text: '二.一', value: '2.1'},
+                {text: '二.二', value: '2.2'}
             ]
         }
-    ])
-    const [selected,setSelected] = useState(['1.1','2.2','2'])
-    const onChange = (item:SourceDataItem,status:boolean)=>{
-        if(status){
-            setSelected([...selected,item.value])
-        }else {
-            setSelected(selected.filter(value => value!==item.value))
+    ]);
+    const [selectedValues, setSelectedValues] = useState(['1.1', '2.2', '2']);
+    const [selectedValue, setSelectedValue] = useState('2');
+    const multiple = false;
+    const onChange = (item: SourceDataItem, status: boolean) => {
+        if (status) {
+            multiple ? setSelectedValues([...selectedValues, item.value]) :
+                setSelectedValue(item.value);
+        } else {
+            multiple ? setSelectedValues(selectedValues.filter(value => value !== item.value)) :
+                setSelectedValue('');
         }
-    }
+    };
 
     return (<>
-            <Tree
-                sourceData={sourceData}
-                selected = {selected}
-                onChange={onChange}
-                multiple={true}
-            />
+        <Tree
+            sourceData={sourceData}
+            selected={selectedValue}
+            onChange={onChange}
+            // multiple={multiple}
+        />
     </>);
-}
+};
 
-export default TreeExample
+export default TreeExample;
--- lib/tree/tree.tsx
+++ lib/tree/tree.tsx
@@ -11,42 +11,40 @@ export interface SourceDataItem {
 type TreeProps = {
     className?: string
     sourceData: SourceDataItem[],
-    selected: string[],
     onChange: (item: SourceDataItem, status: boolean) => void
 } & (
     { selected: string[], multiple: true } |
-    { selected: string, multiple: false }
+    { selected: string, multiple?: false }
     )
 
 const sc = scopedClassMaker('tree');
-const renderItem = (item: SourceDataItem, selected: string[], onChange: (item: SourceDataItem, status: boolean) => void, level = 1) => {
-    return (
-        <div
-            key={item.text}
-            className={sc({['level-' + level]: true})}
 
-        >
-            <div className={sc('item')} style={{paddingLeft: `${(level - 1) * 2}em`}}>
-                <input
-                    type="checkbox"
-                    checked={selected.indexOf(item.value) >= 0}
-                    onChange={(e) => onChange(item, e.target.checked)}
-                />
-                {item.text}
-            </div>
-            {item.children?.map(sub => {
-                return renderItem(sub, selected, onChange, level + 1);
-            })}
-        </div>
-    );
-};
 const Tree: React.FunctionComponent<TreeProps> = (props) => {
-    const {className, onChange, selected, sourceData, ...rest} = props;
-
+    const {className, onChange, selected, sourceData, multiple, ...rest} = props;
+    const renderItem = (item: SourceDataItem, level = 1) => {
+        return (
+            <div
+                key={item.text}
+                className={sc({['level-' + level]: true})}
+            >
+                <div className={sc('item')} style={{paddingLeft: `${(level - 1) * 2}em`}}>
+                    <input
+                        type="checkbox"
+                        checked={multiple ? selected.indexOf(item.value) >= 0 : selected === item.value}
+                        onChange={(e) => onChange(item, e.target.checked)}
+                    />
+                    {item.text}
+                </div>
+                {item.children?.map(sub => {
+                    return renderItem(sub, level + 1);
+                })}
+            </div>
+        );
+    };
     return (<>
         <div className={sc('', className)} {...rest}>
             {sourceData.map((item) => {
-                return renderItem(item, selected, onChange);
+                return renderItem(item);
             })}
         </div>
     </>);
