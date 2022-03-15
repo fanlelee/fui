da8c212d8753b1c43098133e2c4889d3d8e4ce1e
fanlelee <529378687@qq.com>
2022-03-15 11:54:19 +0800

    城市选择组件 雏形

--- lib/citySelect/citySelect.tsx
+++ lib/citySelect/citySelect.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import './citySelect.scss'
-
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './citySelect.scss';
+import {scopedClassMaker} from '../helpers/classes';
+const sc = scopedClassMaker('citySelect')
 
 const CitySelect: React.FunctionComponent = (props) => {
+    const [dialogVisible, setDialogVisible] = useState(false);
     return (<>
-        <div>{props.children}</div>
+        <div onClick={() => {setDialogVisible(true);}}>{props.children}</div>
+        {dialogVisible && <Dialog onClose={()=>setDialogVisible(false)}/>}
     </>);
+}
+
+const Dialog: React.FC<{onClose:()=>void}> = (props) => {
+    return ReactDOM.createPortal(
+        (
+            <div
+                className={sc('dialog')}
+                onClick={props.onClose}
+            >
+                弹窗内容
+            </div>
+        ),
+        document.body);
 };
 
 export default CitySelect;
