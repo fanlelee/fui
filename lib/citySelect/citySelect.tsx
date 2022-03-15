e2dc5d03933467e7fb2ac4f694d1ab11de671197
fanlelee <529378687@qq.com>
2022-03-15 15:04:58 +0800

    CitySelect 弹窗布局

--- lib/citySelect/citySelect.tsx
+++ lib/citySelect/citySelect.tsx
@@ -2,27 +2,41 @@ import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './citySelect.scss';
 import {scopedClassMaker} from '../helpers/classes';
-const sc = scopedClassMaker('citySelect')
+import Icon from '../icon/icon';
+
+const sc = scopedClassMaker('citySelect');
 
 const CitySelect: React.FunctionComponent = (props) => {
-    const [dialogVisible, setDialogVisible] = useState(false);
+    const [dialogVisible, setDialogVisible] = useState(true);
     return (<>
-        <div onClick={() => {setDialogVisible(true);}}>{props.children}</div>
-        {dialogVisible && <Dialog onClose={()=>setDialogVisible(false)}/>}
+        <div className={sc('')} onClick={() => {setDialogVisible(true);}}>{props.children}</div>
+        {dialogVisible && <Dialog onClose={() => setDialogVisible(false)}/>}
     </>);
-}
+};
+const dsc = scopedClassMaker('citySelect-dialog');
+const Dialog: React.FC<{ onClose: () => void }> = (props) => {
+    return ReactDOM.createPortal((
+            <div className={dsc('')}>
+                <header className={dsc('header')}>
+                    <span onClick={props.onClose} className={dsc('back')}><Icon name='left'/></span>
+                    选择城市
+                </header>
+                <div className={dsc('cityBox')}>
+                    <div className={dsc('location')}>
+                        定位城市:
+                        <span>
+                        <Icon name='location' style={{height: '1em', width: '1em'}}/>
+                        成都
+                        </span>
+                    </div>
+                    <div className={dsc('cityList')}>
+                        <h3>全部城市</h3>
 
-const Dialog: React.FC<{onClose:()=>void}> = (props) => {
-    return ReactDOM.createPortal(
-        (
-            <div
-                className={sc('dialog')}
-                onClick={props.onClose}
-            >
-                弹窗内容
+                    </div>
+                </div>
             </div>
-        ),
-        document.body);
+        ), document.body
+    );
 };
 
 export default CitySelect;
